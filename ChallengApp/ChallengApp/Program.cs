12cd9ec5a5857e1b7abc60bcbd55b6a8342c9f9c
ryszard-zone <cyfram@wp.pl>
2024-01-09 17:32:28 +0100

    Lekcja 4.1

--- ChallengApp/ChallengApp/Program.cs
+++ ChallengApp/ChallengApp/Program.cs
@@ -1,15 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Reflection;
-
-var myLab = int.MaxValue - 5;
-int myAge = 33;
-Console.WriteLine(myLab);
-
-int myVar1 = int.MinValue;
-uint myVar2 = int.MaxValue;
-long myVar3 = long.MaxValue;
-var name = "Ewa";
+﻿var name = "Ewa";
 var gender = "female";
 var age = 25;
 
